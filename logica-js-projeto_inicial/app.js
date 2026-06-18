74997ef2366d8cf36eda684b1d91038ff0702224
thiago da costa <thiago99verde@gmail.com>
2026-06-18 14:06:06 -0300

    avanço do curso

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,1 +1,14 @@
-alert ("Olá, Mundo!");
+alert ("Bem vindo ao Jogo Numero secreto!");
+let numeroSecreto = 29;
+console.log("O numero secreto é: " + numeroSecreto);
+let chute = prompt("Digite um numero entre 0 e 100");
+
+//se o chute for igual ao numero secreto
+if (chute == numeroSecreto){
+    alert(`Parabéns, você acertou o numero secreto! ${numeroSecreto}`);
+}
+//se não for igual, o jogador errou
+else{
+    alert("Que pena, você errou!");
+}
+
