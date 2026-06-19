03d0900e7217b9f5212e7f208cfbe1e72ca136db
thiago da costa <thiago99verde@gmail.com>
2026-06-19 10:42:04 -0300

    avanço

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,14 +1,27 @@
 alert ("Bem vindo ao Jogo Numero secreto!");
 let numeroSecreto = 29;
 console.log("O numero secreto é: " + numeroSecreto);
-let chute = prompt("Digite um numero entre 0 e 100");
+let chute;
+let tentativas = 1;
 
-//se o chute for igual ao numero secreto
-if (chute == numeroSecreto){
-    alert(`Parabéns, você acertou o numero secreto! ${numeroSecreto}`);
-}
-//se não for igual, o jogador errou
-else{
-    alert("Que pena, você errou!");
+//enquanto o chute for diferente do numero secreto, o jogador erra
+while(chute != numeroSecreto){
+    chute = prompt("Digite um numero entre 0 e 100");  
+    //se o chute for igual ao numero secreto
+    if (chute == numeroSecreto){
+        alert(`Parabéns, você acertou o numero secreto! ${numeroSecreto} com ${tentativas} tentativas!`);
+}   //se não for igual, o jogador errou
+        else{
+                alert("Que pena, você errou!");
+             if(numeroSecreto > chute){
+                alert(`O numero secreto é maior que ${chute}!`);
+        }
+                else{
+                    alert(`O numero secreto é menor que ${chute}!`);
+        }
+
+         tentativas++;
+    }
+
 }
 
