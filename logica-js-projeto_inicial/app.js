707b70a9058aea86884022e0a31c920b2e8b59a8
thiago da costa <thiago99verde@gmail.com>
2026-06-24 16:04:21 -0300

    adicionado aleatoriedade no numero gerado

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,5 +1,5 @@
 alert ("Bem vindo ao Jogo Numero secreto!");
-let numeroSecreto = 29;
+let numeroSecreto = parseInt(Math.random() * 100) + 1;
 console.log("O numero secreto é: " + numeroSecreto);
 let chute;
 let tentativas = 1;
@@ -7,13 +7,16 @@ let tentativas = 1;
 //enquanto o chute for diferente do numero secreto, o jogador erra
 while(chute != numeroSecreto){
     chute = prompt("Digite um numero entre 0 e 100");  
+
     //se o chute for igual ao numero secreto
     if (chute == numeroSecreto){
-        alert(`Parabéns, você acertou o numero secreto! ${numeroSecreto} com ${tentativas} tentativas!`);
-}   //se não for igual, o jogador errou
+        break;       }   
+
+//se não for igual, o jogador errou
         else{
                 alert("Que pena, você errou!");
-             if(numeroSecreto > chute){
+
+                if(numeroSecreto > chute){
                 alert(`O numero secreto é maior que ${chute}!`);
         }
                 else{
@@ -25,3 +28,15 @@ while(chute != numeroSecreto){
 
 }
 
+let palavraTentativas = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Parabéns, você acertou o numero secreto! ${numeroSecreto} com ${tentativas} ${palavraTentativas}!`);
+
+// if (tentativas > 1 ){
+
+//         alert(`Parabéns, você acertou o numero secreto! ${numeroSecreto} com ${tentativas} tentativas!`);
+// }
+
+//  else{
+
+//         alert(`Parabéns, você acertou o numero secreto! ${numeroSecreto} com ${tentativas} tentativa!`);   
+//  }
